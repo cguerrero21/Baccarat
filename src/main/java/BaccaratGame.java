d247f09eee834387c13e28083485fe8ada74a348
cguerrero21 <guerrero1005@hotmail.com>
2023-10-29 18:34:38 -0500

    Seventh Commit

--- src/main/java/BaccaratGame.java
+++ src/main/java/BaccaratGame.java
@@ -2,15 +2,38 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 
 public class BaccaratGame extends Application {
 
+	private ArrayList<Card> playerHand;
+	private ArrayList<Card> bankerHand;
+	private BaccaratDealer theDealer;
+	private BaccaratGameLogic gameLogic;
+	private double currentBet;
+	private double totalWinnings;
 
+	public BaccaratGame(){
+		playerHand = new ArrayList<Card>();
+		bankerHand = new ArrayList<Card>();
+		theDealer = new BaccaratDealer();
+		gameLogic = new BaccaratGameLogic();
+	}
+
+	public double evaluateWinnings(){
+
+		return 0;
+	}
 
 
 
@@ -26,6 +49,16 @@ public class BaccaratGame extends Application {
 
 		Image background= new Image("file:baccarat-table-layout.jpg");
 		Group root = new Group();
+
+		BorderPane layout = new BorderPane();
+		MenuBar menuBar = new MenuBar();
+		Menu options = new Menu("Options");
+		menuBar.getMenus().add(options);
+		layout.setTop(menuBar);
+
+		MenuItem exit = new MenuItem("Exit");
+		MenuItem fresh = new MenuItem("Fresh Start");
+		options.getItems().addAll(exit, fresh);
 
 		ImageView mv = new ImageView(background);
 		mv.setPreserveRatio(true);
@@ -45,7 +78,9 @@ public class BaccaratGame extends Application {
 		StackPane.setMargin(clear, new javafx.geometry.Insets(-475, 0, 0, -550));
 		StackPane.setMargin(deal, new javafx.geometry.Insets(-475, -550, 0, 0));
 
-		Scene scene = new Scene(stackPane, 1200, 690);
+		layout.setCenter(stackPane);
+
+		Scene scene = new Scene(layout, 1200, 690);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
