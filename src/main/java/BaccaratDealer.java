5f79277c22830e8add1d6d54e415aa69001cfb00
cguerrero21 <guerrero1005@hotmail.com>
2023-10-28 23:27:03 -0500

    Third commit

--- src/main/java/BaccaratDealer.java
+++ src/main/java/BaccaratDealer.java
@@ -1,2 +1,56 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BaccaratDealer {
+    private ArrayList<Card> deck;
+
+    public BaccaratDealer(){
+        deck = new ArrayList<Card>();
+    }
+
+    public void generateDeck(){
+        for (int i = 1; i <= 13; i++){
+            Card card = new Card("Spade", i);
+            deck.add(card);
+        }
+        for (int i = 1; i <= 13; i++){
+            Card card = new Card("Clubs", i);
+            deck.add(card);
+        }
+        for (int i = 1; i <= 13; i++){
+            Card card = new Card("Diamond", i);
+            deck.add(card);
+        }
+        for (int i = 1; i <= 13; i++){
+            Card card = new Card("Heart", i);
+            deck.add(card);
+        }
+    }
+    //-----------------------------------------------------
+    public ArrayList<Card>dealHand(){
+        ArrayList<Card> hand = new ArrayList<Card>();
+        Card firstCard = deck.get(0);
+        Card secondCard = deck.get(1);
+        hand.add(firstCard);
+        hand.add(secondCard);
+        deck.remove(0);
+        deck.remove(0);
+        return hand;
+    }
+    //-----------------------------------------------------
+    public ArrayList<Card> drawOne(){
+        ArrayList<Card> hand = new ArrayList<Card>();
+        Card card = deck.get(0);
+        hand.add(card);
+        deck.remove(0);
+        return hand;
+    }
+    //-----------------------------------------------------
+    public void shuffleDeck(){
+        Collections.shuffle(deck);
+    }
+    //-----------------------------------------------------
+    public int deckSize(){
+        return deck.size();
+    }
 }
--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -4,15 +4,7 @@ public class Card {
     private int value; // = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
     public Card(String theSuite, int theValue){
-        if (theSuite.equals("spade") || theSuite.equals("clove") || theSuite.equals("diamond") || theSuite.equals("heart")){
-            this.suite = theSuite;
-        }else{
-            System.out.println("Not a valid suite!\n");
-        }
-        if (theValue >= 10){
-            this.value = 0;
-        }else{
-            this.value = theValue;
-        }
+        suite = theSuite;
+        value = theValue;
     }
 }
