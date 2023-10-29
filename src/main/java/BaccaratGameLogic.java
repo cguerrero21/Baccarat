2e6e3a3c7c597064fc5ece9c2f090d9893a8dae1
cguerrero21 <guerrero1005@hotmail.com>
2023-10-29 15:53:33 -0500

    Fifth Commit

--- src/main/java/BaccaratGameLogic.java
+++ src/main/java/BaccaratGameLogic.java
@@ -2,31 +2,40 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class BaccaratGameLogic {
+    // This class is used to decided the logic behind the game and check for moves that might need to be
+    // done or to evaluate the conclusion/winner of the game;
     public String whoWon(ArrayList<Card> hand1, ArrayList<Card> hand2){
+        int dealer = handTotal(hand1);
+        int player = handTotal(hand2);
 
+        if ((player == 9 || player == 8) && (player != dealer)){
+            return "Player";
+        }else if ((dealer == 9 || dealer == 8) && (player != dealer)){
+            return "Dealer";
+        }else if (player == dealer){
+            return "Tie";
+        }else if (dealer > player){
+            return "Dealer";
+        }else{
+            return "Player";
+        }
     }
     //-----------------------------------------
     public int handTotal(ArrayList<Card> hand){
         int total = 0;
 
-        for (int i = 0; i <= hand.size(); i++){
-            if (hand.get(i).getValue() >= 10){
-
+        for (Card c: hand){
+            int value = c.getValue();
+            if (value <= 9 && value >= 1){
+                total += value;
+            }else if (value >= 10 && value <=13){
+                total += 0;
             }
         }
-
-
-        if (hand.size() == 3){
-            for (int i = 0; i <= (hand.size()-1); i++){
-                total = total + hand.get(i).getValue();
-            }
-            if (total > 10){
-                total = total - 10;
-            }
-            total = total + hand.get(2).getValue();
-        }else{
-
+        if (total > 9){
+            total = total - 10;
         }
+        return total;
     }
     //-----------------------------------------
     public boolean evaluateBankerDraw(ArrayList<Card> hand, Card playerCard){
